f516d516d12b14ebacbbbf883107bb5aaf1abb29
Nikhil Arora <narora200@gmail.com>
2017-07-30 14:35:11 +0530

    Basic-example: Basic routing implemented

--- basic-example/src/App.js
+++ basic-example/src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Topics from './components/Topics/Topics';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ basic-example/src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './About.css';
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+export default About;
