2ebb28381cc245016bfed84548bfb3bf8cb56da9
Nikhil Arora <narora200@gmail.com>
2017-07-30 14:57:31 +0530

    Basic-example: Basic routing implemented with sub-routes in Topic

--- basic-example/src/components/Topics/Topics.js
+++ basic-example/src/components/Topics/Topics.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import './Topics.css';
 
-const Topics = () => (
+import { Link, Route } from 'react-router-dom';
+
+
+
+const Topics = ({ match }) => (
 
   <div>
     <h2>Topics</h2>
+    <ul>
+      <li>
+          <Link to={`${match.url}/rendering`}>
+            Rendering with react
+          </Link>
+      </li>
+      <li>
+          <Link to={`${match.url}/components`}>
+            Component
+          </Link>
+      </li>
+      <li>
+          <Link to={`${match.url}/props`}>
+            Props
+          </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route exact path={`${match.url}`} render={() => (
+      <h3>Please select a topic...</h3>
+    )} />
+
+  </div>
+)
+
+const Topic = ({ match }) => (
+
+  <div>
+    <h3>{match.params.topicId}</h3>
   </div>
 )
 
