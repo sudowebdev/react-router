7945e87bc7847e72dd51807481e9aa73eb587a48
Nikhil Arora <narora200@gmail.com>
2017-08-01 17:52:24 +0530

    redirects-auth: fakeAuth object (signout method added)

--- 03-redirects-auth/src/components/AuthExample/AuthExample.js
+++ 03-redirects-auth/src/components/AuthExample/AuthExample.js
@@ -39,6 +39,10 @@ const fakeAuth = {
   authenticate(cb) {
     this.isAuthenticated = true;
     setTimeout(cb, 100); //fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
   }
 }
 
