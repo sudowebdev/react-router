f72cc80e4dc32559e3200fd1fd8483592e86deda
Nikhil Arora <narora200@gmail.com>
2017-08-05 23:13:20 +0530

    :construction_worker: 04-custom-link: OldSchoolMenuLink component (Comments added)

--- 04-custom-link/src/components/CustomLink/CustomLink.js
+++ 04-custom-link/src/components/CustomLink/CustomLink.js
@@ -20,6 +20,14 @@ const CustomLinkExample = () => (
 )
 
 const OldSchoolMenuLink = ( { activeOnlyWhenExact, to, label } ) => (
+
+  {/*
+      Route with children is used in case we want to render something
+      even if there isn't any match.
+      When a route fails to match the URL, then match is null.
+      For more information: https://reacttraining.com/react-router/web/api/Route/children-func
+  */}
+
   <Route path={to} exact={activeOnlyWhenExact} children={ ( { match } ) => (
     <div className={match ? 'active' : ''}>
       {match ? '> ' : ''}<Link to={to}>{label}</Link>
