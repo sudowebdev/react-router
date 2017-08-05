8dca0f8abf05fe27348bfcfcc94b2240e3191e25
Nikhil Arora <narora200@gmail.com>
2017-08-05 23:07:20 +0530

    :construction_worker: 04-custom-link: CustomLinkExample component defined

--- 04-custom-link/src/components/CustomLink/CustomLink.js
+++ 04-custom-link/src/components/CustomLink/CustomLink.js
@@ -5,3 +5,16 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+
+const CustomLinkExample = () => (
+  <Router>
+    <div>
+      <OldSchoolMenuLink activeOnlyWhenExact={true} to="/" label="Home" />
+      <OldSchoolMenuLink to="/about" label="About" />
+
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  </Router>
+)
